d6c068feebd95cc62aef44f9459025e4aef64f1d
wujianbiao <18392371793@163.com>
2022-08-31 18:12:25 +0800

    add dao module

---
+++ diet-dao/src/test/java/com/wujianbiao/diet/DietDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wujianbiao.diet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietDaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- diet-model/src/main/java/com/wujianbiao/diet/DietPo.java
+++ diet-model/src/main/java/com/wujianbiao/diet/DietPo.java
@@ -1,5 +1,6 @@
 package com.wujianbiao.diet;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import lombok.Data;
@@ -10,11 +11,18 @@ import lombok.Data;
  * @CreateTime 2022/8/29 5:51 PM
  **/
 @Data
-public class DietPo {
+//@Entity
+//@Table(name = "diet")
+public class DietPo implements Serializable {
+
+    // todo 可以不显式实现序列化吗？
+    private static final long serialVersionUID = -2480019952655534636L;
 
     // todo 使用 MySQL 吧
-    private Integer id;
+//    @GeneratedValue
+    private Long id;
 
+    //    @Column(name = "name")
     private String name;
 
     private Date createTime;
